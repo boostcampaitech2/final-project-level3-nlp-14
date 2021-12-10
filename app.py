684400ec4a89261107cb2cdd5adbec3bb75bc97f
jinmang2 <jinmang2@gmail.com>
2021-12-10 15:23:41 +0000

    update backend baseline

---
+++ app.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('./vqa/bottom_up_attention_pytorch/detectron2')
+sys.path.append('./vqa/bottom_up_attention_pytorch/')
+
+import uvicorn
+from fastapi import FastAPI
+from model import QAManager
+from version import VERSION
+from constants import (
+    Union, Dict,
+    Input,
+    TITLE, DESCRIPTION
+)
+
+
+app = FastAPI(
+    title=TITLE,
+    description=DESCRIPTION,
+    version=VERSION,
+)
+qa_manager = QAManager()
+
+
+@app.post("/chat")
+def chat(
+    input: Input,
+) -> Dict[str, str]:
+    query = input.query
+    document = input.document
+    image = input.image
+    answer, answerable = qa_manager(query, document, image)
+    if not answerable:
+        answer = "질문에 답하기가 어려워요 ㅠㅠ"
+    return {"answer": answer}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port="8000")
---
+++ constants.py
@@ -0,0 +1,20 @@
+from typing import Optional, Union, Dict
+from PIL.Image import Image
+
+
+from pydantic import BaseModel
+
+
+class Input(BaseModel):
+    query: str
+    document: Optional[str]
+    image: Union[Image, str]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+TITLE = "KiYoung2Bot"
+DESCRIPTION = """
+읽거나 보고, 아는 것만 답변하는 지혜로운 기영이봇
+""".strip()
