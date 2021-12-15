52e5fb7369c6fcb332ef31c3b219e14594f440d8
Haram Lee <twinha0204@gmail.com>
2021-12-15 11:40:11 +0000

    fix error : allow CORS

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ sys.path.append('./vqa/bottom_up_attention_pytorch/')
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model import QAManager
 from version import VERSION
 from constants import (
@@ -19,6 +20,17 @@ app = FastAPI(
     version=VERSION,
 )
 qa_manager = QAManager()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/chat")
