8df570e7b60d0af8278b056e83a5bb8080ee64d7
jinmang2 <jinmang2@gmail.com>
2021-12-18 10:34:19 +0000

    upload today's work - concat dialogue

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-import sys
-sys.path.append('./vqa/bottom_up_attention_pytorch/detectron2')
-sys.path.append('./vqa/bottom_up_attention_pytorch/')
+import re
+# import sys
+# sys.path.append('./vqa/bottom_up_attention_pytorch/detectron2')
+# sys.path.append('./vqa/bottom_up_attention_pytorch/')
+
+import logging
 
 from PIL import Image
 from typing import Optional
@@ -16,12 +19,21 @@ from constants import (
 )
 
 
+# Set logger
+logging.basicConfig(filename="app.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logging.info("Running kiyoung2 app")
+logger = logging.getLogger(__name__)
+
+# Set up FastAPI
 app = FastAPI(
     title=TITLE,
     description=DESCRIPTION,
     version=VERSION,
 )
-qa_manager = QAManager()
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -34,6 +46,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Set up Question Answering Manager
+qa_manager = QAManager()
+
+# @Deprecated: Set threshold
+thresh: float = 0.5
+
 
 @app.post("/chat")
 async def chat(
@@ -41,6 +59,10 @@ async def chat(
     document: Optional[str] = Form(None),
     image: Optional[UploadFile] = File(None),
 ) -> Dict[str, str]:
+    """ """
+    # Preprocess query
+    query = re.sub("\?", "", query)
+    # Process Context
     if image is not None and image.filename != "":
         contents = await image.read()
         with open(f"tmp/{image.filename}", "wb") as f:
@@ -49,10 +71,19 @@ async def chat(
     else:
         img = None
     
-    answer, answerable = qa_manager(query, document, img)
-    if not answerable:
-        answer = "질문에 답하기가 어려워요 ㅠㅠ"
-    answers = [answer]
+    # Identify whether knowledge is needed
+    qa_ratio = qa_manager.identify_intent(query)
+    
+    if qa_ratio >= thresh:
+        answer, answerable = qa_manager.answer(query, document, img)
+        chatbot_input = "<answer>" if answerable else "<noanswer>"
+        response = qa_manager.chat(chatbot_input)
+        answers = [answer, response]
+    else:
+        response = qa_manager.chat(query, thresh=thresh)
+        answers = [response]
+    logging.info(f"query: {query}\ttype(doc){type(document)}\ttype(img){type(img)}\t"
+                 f"qa_ratio: {qa_ratio}\tthresh: {thresh}\tanswers: {answers}")
     return answers
 
 
--- constants.py
+++ constants.py
@@ -18,3 +18,15 @@ TITLE = "KiYoung2Bot"
 DESCRIPTION = """
 읽거나 보고, 아는 것만 답변하는 지혜로운 기영이봇
 """.strip()
+
+TOKENIZER_USER_AGENT = {
+    "file_type": "tokenizer", 
+    "from_auto_class": False,
+    "is_fast": False,
+}
+
+STATIC_FALLBACK_MESSAGES = [
+    "무슨 말인지 잘 모르겠어...",
+    "내가 알아들을 수 있게 잘 말해줘!",
+    "잘 이해가 안되는데 좀 더 자세하게 말해주라 ㅎㅎ",
+]
